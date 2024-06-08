54c4a1d05090ca90c89eda7ae48b21bc43e742c4
msw6468 <c16439@gmail.com>
2024-06-08 15:27:23 +0900

    init commit

---
+++ single_modality/models/extract_clip/clip_install.py
@@ -0,0 +1,14 @@
+import clip.clip as clip
+import os
+import torch
+from collections import OrderedDict
+
+path = '/gallery_moma/sangwoo.moon/umt_filter/multi_modality/pretrained_model/clip_visual_encoder' # SNU server
+# path = '/net/nfs3.prior/dongjook/pretrained_model/clip_visual_encoder' # AI2 server
+
+model, _ = clip.load("ViT-L/14", device='cpu')
+new_state_dict = OrderedDict()
+for k, v in model.state_dict().items():
+    if 'visual.' in k:
+        new_state_dict[k[7:]] = v
+torch.save(new_state_dict, os.path.join(path, 'vit_l14.pth'))
